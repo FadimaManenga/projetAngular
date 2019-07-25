d96b3be1b78c7619f0d298c84994c9380aafdeae
Fadima <manenga@gmail.com>
2019-07-26 01:56:31 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mon-projet02-angular';
+  title = 'my first Angular SPA';
 }
